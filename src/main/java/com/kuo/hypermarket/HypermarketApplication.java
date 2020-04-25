24442bd5b42924098a0316738670e5b87a4c265a
qygxwy <qygxwy@163.com>
2020-04-25 12:01:18 +0800

    创建项目01

---
+++ src/main/java/com/kuo/hypermarket/HypermarketApplication.java
@@ -0,0 +1,13 @@
+package com.kuo.hypermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypermarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuo/hypermarket/HypermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuo.hypermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
